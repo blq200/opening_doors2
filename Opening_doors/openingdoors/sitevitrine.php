2ce8706f46d5a77c48b21d4b9b3cbd8c4e4f5f13
Rania_Elbalq <92025169+blq200@users.noreply.github.com>
2022-06-18 17:25:45 +0100

    Add files via upload

---
+++ Opening_doors/openingdoors/sitevitrine.php
@@ -0,0 +1,18 @@
+<?php include('navbar.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h3>My Map</h3>
+    <div id="map"></div>
+    <script async defer src ="https://googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"></script>
+    <script src="script.js"></script>
+</body>
+</html>
